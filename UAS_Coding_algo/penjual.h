d9677a4a46b35d4eface58ad86efb0002ad873ff
Bryan Chandra <141706126+BceHe@users.noreply.github.com>
2024-12-16 20:16:51 +0700

    Update penjual.h

--- UAS_Coding_algo/penjual.h
+++ UAS_Coding_algo/penjual.h
@@ -1,4 +1,36 @@
-#IFNDEF PENJUAL_H
-#DEFINE PENJUAL_H
+#ifndef PENJUAL_H
+#define PENJUAL_H
+
+#include <string>
+#include <vector>
+
+struct Menu {
+    std::string nama;
+    double harga;
+    std::string linkFoto;
+};
+
+struct AkunPenjual {
+    std::string gmail;
+    std::string whatsappBusiness;
+    std::string metodePembayaran;
+    std::string namaGerobak;
+    std::string jamOperasional;
+    std::string lokasi;
+    std::vector<Menu> menuItems;
+    bool gpsAccess;
+    bool layananAntar;
+    double jarakMinimum;
+    double jarakMaksimum;
+    double biayaOngkos;
+};
+
+// Fungsi untuk menambah menu
+void tambahMenu(std::vector<Menu>& menuItems);
+
+// Fungsi untuk membuat akun penjual
+void buatAkunPenjual();
+
+#endif
 
 
