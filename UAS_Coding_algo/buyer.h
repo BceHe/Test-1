340d01e1d312630724858dc1fd5a2103b681cbe4
startregic <151872469+startregic@users.noreply.github.com>
2024-12-16 21:51:41 +0700

    Update buyer.h

--- UAS_Coding_algo/buyer.h
+++ UAS_Coding_algo/buyer.h
@@ -6,7 +6,13 @@
 #include <cmath>
 using namespace std;
 
+struct Location {
+    double latitude;
+    double longitude;
+};
+
 double calculateDistance(Location loc1, Location loc2)
+
 void buyerMenu(string loggedInUser);
 
 #endif
