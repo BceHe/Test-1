ac59e3366bd9a7f1a57ab10fe4ea077bc2fe6702
startregic <151872469+startregic@users.noreply.github.com>
2024-12-16 21:20:15 +0700

    Update main.cpp

--- UAS_Coding_algo/main.cpp
+++ UAS_Coding_algo/main.cpp
@@ -39,42 +39,6 @@ double calculateDistance(Location loc1, Location loc2) {
     return R * c;
 }
 
-void sellerMenu(string loggedInUser) {
-    cout << "Menu Penjual (" << loggedInUser << ")" << endl;
-
-    char activateGPS;
-    cout << "Apakah Anda ingin mengaktifkan GPS? (y/n): ";
-    cin >> activateGPS;
-    if (activateGPS == 'y' || activateGPS == 'Y') {
-        activateGPSForPenjual(loggedInUser);
-    }
-
-    char receiveOrder;
-    cout << "Apakah Anda menerima pesanan baru? (y/n): ";
-    cin >> receiveOrder;
-    if (receiveOrder == 'y' || receiveOrder == 'Y') {
-        cout << "Pesanan diterima. Anda dapat memulai memasak." << endl;
-    
-        cout << "Menghubungi pembeli via WhatsApp untuk konfirmasi lebih lanjut..." << endl;
-
-        double distanceToBuyer;
-        cout << "Masukkan jarak Anda ke pembeli dalam meter: ";
-        cin >> distanceToBuyer;
-        if (distanceToBuyer <= 100) {
-            cout << "Pembeli sudah dekat. Order dapat diselesaikan." << endl;
-        } else {
-            cout << "Pembeli belum cukup dekat. Menunggu konfirmasi." << endl;
-        }
-
-        char cancelOrder;
-        cout << "Apakah pembeli membatalkan pesanan? (y/n): ";
-        cin >> cancelOrder;
-        if (cancelOrder == 'y' || cancelOrder == 'Y') {
-            cout << "Pesanan dibatalkan. Anda tetap akan menerima pembayaran." << endl;
-        }
-    }
-}
-
 void mainMenu() {
     int choice;
     do {
