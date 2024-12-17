d203ace89d0f9d128f4b3e5fc162e193047cc1c3
Bryan Chandra <141706126+BceHe@users.noreply.github.com>
2024-12-17 10:15:38 +0700

    Update account.h

--- UAS_Coding_algo/account.h
+++ UAS_Coding_algo/account.h
@@ -8,6 +8,13 @@
 #include <string>
 using namespace std;
 
+struct Account {
+    string username;
+    string password;
+    string whatsapp;
+    string gmail;
+};
+
 void createAccount(const string& userType);
 bool login(const string& userType, string& loggedInUser);
 
